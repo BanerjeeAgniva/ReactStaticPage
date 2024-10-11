9ec80c2701d06c7e7ffcc9f79b37ce83dbefeb65
Agniva Banerjee <agniva.banerjee@harness.io>
2024-10-12 00:17:44 +0530

    s

--- components/Main.js
+++ components/Main.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-export default function Main() {
-    return (
-        <main>
+const mainJSX=(
+    <main>
             <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
@@ -12,5 +11,9 @@ export default function Main() {
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </main>
+)
+export default function Main() {
+    return (
+        mainJSX
     )
 }
